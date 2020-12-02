0c1d1fe1927cce0bff37830f3ca8167018718970
IvanMolin <molin-nso@yandex.ru>
2020-12-02 22:10:32 +0700

    1 урок/1 задание (исправленный вариант)

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -7,12 +7,9 @@ public class MainClassTest
     public void getLocal ()
     {
         int a = Main.getLocalNumber();
-        if (a == 14)
+        if (a != 14)
         {
-            System.out.println("true");
-        }
-        else {
-            System.out.println("False");
+           Assert.fail("a != 14");
         }
     }
 
